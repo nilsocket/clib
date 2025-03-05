1b99b8f7dab64aa90c7a447ca6bc7b35dad637a1
nilsocket <nilsocket@gmail.com>
2025-03-06 02:54:59 +0530

    use marshal instead of encoder, due to addition of empty lines

--- chttp/response.go
+++ chttp/response.go
@@ -14,23 +14,31 @@ import (
 )
 
 type Response struct {
-	Data  any    `json:"data"`
-	Error string `json:"error,omitempty"`
+	Data  any
+	Error string `json:",omitempty"`
 }
 
 // Send writes given `data` or `err` to `w` as `json`
 func Send(w http.ResponseWriter, data any, err error) {
 
 	resp := Response{
-		Data:  data,
-		Error: err.Error(),
+		Data: data,
 	}
 
 	if err != nil {
 		resp.Data = nil
+		resp.Error = err.Error()
 	}
 
-	json.NewEncoder(w).Encode(resp)
+	jsonData, err := json.Marshal(resp)
+	if err != nil {
+		resp.Data = nil
+		resp.Error = err.Error()
+
+		jsonData, _ = json.Marshal(resp)
+	}
+
+	w.Write(jsonData)
 }
 
 // SendAsAttachment attaches given `data` as downloadable attachment with given `filename`
